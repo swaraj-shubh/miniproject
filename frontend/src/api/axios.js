6e2014ea93d7ff09ac3a2d434122425bdef34771
kali <shubhh.ab@gmail.com>
2025-07-30 17:37:21 +0530

    backend url updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: import.meta.env.URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.URL || "http://localhost:8000/api" || "https://secondserve.onrender.com/api",
   withCredentials: true, // if you're using cookies for auth
 });
 
